2251e71902abde46cf6a9ed6970f2e075c5019a4
shuhey <nishizawa@timedia.co.jp>
2019-09-09 20:26:01 +0900

    Add project files.

---
+++ DoneIt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DoneIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
