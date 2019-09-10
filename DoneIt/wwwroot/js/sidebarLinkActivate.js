1d47b099b009ca52b5d5631a97f16e770d975926
shuhey <nishizawa@timedia.co.jp>
2019-09-10 21:15:54 +0900

    Add Sidebar link activate javascript.

---
+++ DoneIt/wwwroot/js/sidebarLinkActivate.js
@@ -0,0 +1,10 @@
+﻿$('a').on('click', function () {
+    //deactivate all link in sidebar
+    $('#sidebar_body').find('a')
+        .each(function () {
+            $(this).removeClass('is-active');
+        });
+
+    //activate clicked link
+    $(this).addClass('is-active');
+});
