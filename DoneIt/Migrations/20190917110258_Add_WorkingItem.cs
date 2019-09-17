a7121e9393c7ad7e59f8db7dfdc8fc1a200ebc4a
shuhey <nishizawa@timedia.co.jp>
2019-09-17 20:11:04 +0900

    Add WorkingItem and migrate it.

---
+++ DoneIt/Migrations/20190917110258_Add_WorkingItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DoneIt.Migrations
+{
+    public partial class Add_WorkingItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkingItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    From = table.Column<DateTimeOffset>(nullable: false),
+                    To = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkingItem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkingItem");
+        }
+    }
+}
---
+++ DoneIt/Models/WorkingItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoneIt.Models
+{
+    public class WorkingItem
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTimeOffset From { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTimeOffset To { get; set; }
+    }
+}
