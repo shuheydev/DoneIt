4605d24f629c3a14dbb0db704435831a52d7355b
shuhey <nishizawa@timedia.co.jp>
2019-09-17 20:07:56 +0900

    Add Identity and migrate it.

---
+++ DoneIt/Models/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoneIt.Models
+{
+    public class AppDbContext : IdentityDbContext //IdentityDbContextクラスを継承
+    {
+        //コンストラクタ
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        //テーブルのモデル（クラス）を指定する
+        //DbSet<Movie> Movie {get; set;} //例
+        DbSet<WorkingItem> WorkingItem { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
+
+//チュートリアル65~
